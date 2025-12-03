e575847f19c4bdfea0b097562e0e6b0d1856626b
sirhCC <sirhCC@users.noreply.github.com>
2025-12-02 23:53:13 -0500

    Update Jest configuration to specify test roots and match patterns for test files

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
-  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
   collectCoverageFrom: [
     'src/**/*.ts',
     '!src/**/*.test.ts',
