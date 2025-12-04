5f8dae882e96b8cf25976d3ef83ae2adfb3372b8
sirhCC <sirhCC@users.noreply.github.com>
2025-12-03 19:59:23 -0500

    Add missing exports for KubernetesPlugin and DockerPlugin in index.ts

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,3 +1,5 @@
 export { MockPlugin } from './mock-plugin';
 export { AWSPlugin } from './aws-plugin';
+export { KubernetesPlugin } from './kubernetes-plugin';
+export { DockerPlugin } from './docker-plugin';
 export { PluginManager } from './plugin-manager';
